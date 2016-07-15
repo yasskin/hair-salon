97f52ab2082d8474fa44a33ebc3cca582720c573
Noah <yasskin@icloud.com>
2016-07-15 14:59:02 -0700

    add first integration spec and front end index and stylist_success and stylist erb pages to make the spec pass

--- app.rb
+++ app.rb
@@ -8,7 +8,22 @@ also_reload('lib/**/*.rb')
 DB = PG.connect({:dbname => 'hair_salon_test'})
 
 get('/') do
+  @stylists = Stylist.all()
+  @clients = Client.all()
+  erb(:index)
+end
 
+get('/stylist/:id') do
+  @stylist = Stylist.find(params.fetch('id').to_i())
+  @client = Clients.all()
+  erb(:stylist)
+end
+
+post('/stylist') do
+  @name = params.fetch('name')
+  train = Stylist.new({:name => @name})
+  train.save()
+  erb(:stylist_success)
 end
 
 get('/stylist/:id') do
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,3 +2,59 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('adding a stylist', {:type => :feature}) do
+  it('adds a stylist to the list of stylists') do
+    visit('/')
+    fill_in('name', :with => 'Zohan')
+    click_button('Add stylist')
+    expect(page).to have_content('Success!')
+  end
+end
+
+# describe('displaying a list of stylists once they have been created', {:type => :feature}) do
+#   it('reads back a list of stylists') do
+#   visit('/')
+#   fill_in('name', :with => 'Zohan')
+#   click_button('Add stylist')
+#   click_link('Return to dashboard')
+#   expect(page).to have_content('Zohan')
+#   end
+#   it('turns list item into a link that leads to a stylist page') do
+#     visit('/')
+#     fill_in('name', :with => 'Zohan')
+#     click_button('Add stylist')
+#     click_link('Return to dashboard')
+#     click_link('Zohan')
+#     expect(page).to have_content('Delete Zohan')
+#   end
+# end
+#
+# describe('allows user to delete a stylist they have added', {:type => :feature}) do
+#   it('adds a stylist to the list of stylists, then deletes it') do
+#     visit('/')
+#     fill_in('name', :with => 'Zohan')
+#     click_button('Add stylist')
+#     click_link('Return to dashboard')
+#     fill_in('name', :with => 'Freight')
+#     click_button('Add stylist')
+#     click_link('Return to dashboard')
+#     click_link('Zohan')
+#     click_button('Delete Zohan')
+#     expect(page).to have_no_content('Zohan')
+#   end
+# end
+#
+# describe('adding a client', {:type => :feature}) do
+#   it('adds a client stop to the list of cities') do
+#     visit('/')
+#     fill_in('name', :with => 'Zohan')
+#     click_button('Add stylist')
+#     click_link('Return to dashboard')
+#     click_link('Zohan')
+#     fill_in('client-name', :with => "Rey")
+#     click_button('Add Stop')
+#     click_link('Zohan')
+#     expect(page).to have_content('Rey')
+#   end
+# end
