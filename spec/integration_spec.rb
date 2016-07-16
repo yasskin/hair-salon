c1af865b940da3ff81f7ef0f0c96da1d882dfe9d
Noah <yasskin@icloud.com>
2016-07-16 10:55:19 -0700

    adds functionality to display a list of stylists on the home page once they have been created

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -12,23 +12,23 @@ describe('adding a stylist', {:type => :feature}) do
   end
 end
 
-# describe('display a list of stylists once they have been created', {:type => :feature}) do
-#   it('reads back a list of stylists') do
-#   visit('/')
-#   fill_in('name', :with => 'Zohan')
-#   click_button('Add Stylist')
-#   click_link('Return to dashboard')
-#   expect(page).to have_content('Zohan')
-#   end
-#   # it('turns list item into a link that leads to a stylist page') do
-#   #   visit('/')
-#   #   fill_in('name', :with => 'Zohan')
-#   #   click_button('Add Stylist')
-#   #   click_link('Return to dashboard')
-#   #   click_link('Zohan')
-#   #   expect(page).to have_content('Delete Zohan')
-#   # end
-# end
+describe('display a list of stylists once they have been created', {:type => :feature}) do
+  it('reads back a list of stylists') do
+  visit('/')
+  fill_in('stylist-name', :with => 'Zohan')
+  click_button('Add Stylist')
+  click_link('Return to dashboard')
+  expect(page).to have_content('Zohan')
+  end
+  # it('turns list item into a link that leads to a stylist page') do
+  #   visit('/')
+  #   fill_in('name', :with => 'Zohan')
+  #   click_button('Add Stylist')
+  #   click_link('Return to dashboard')
+  #   click_link('Zohan')
+  #   expect(page).to have_content('Delete Zohan')
+  # end
+end
 #
 # describe('allows user to delete a stylist they have added', {:type => :feature}) do
 #   it('adds a stylist to the list of stylists, then deletes it') do
